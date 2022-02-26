12d78a52186074ecda1cda26fc802a10dbb95e59
nikeybellic <mannyolowolafe@gmail.com>
2022-02-26 14:35:34 -0500

    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/nikeybellic"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out my first React App!
         </a>
       </header>
     </div>
